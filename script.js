33d70be8ab32fe570480675e602e3ecd16673d54
Tanmay Singh <tanmay.singh@incruiter.com>
2023-12-05 05:13:52 +0530

    addition of desktop css and second implementation of show-more functionality

--- script.js
+++ script.js
@@ -1,30 +1,81 @@
+//function showMore() {
+//    var mainContent = document.getElementById('mainContent');
+//    var hiddenContent = document.getElementById('hiddenContent');
+//    var showmoreButton = document.getElementById('showButton');
+//    if (hiddenContent.textContent.length == 0) {
+//        hiddenContent.textContent = mainContent.textContent.slice(0, mainContent.textContent.length);
+//        mainContent.textContent = '...';
+//   //     mainContent.textContent = mainContent.textContent.slice(0, 100) + '...';
+//        showmoreButton.innerHTML = 'Show More <i class="fas fa-arrow-down"></i>'
+//    }
+//    var maintext = hiddenContent.textContent;
+//    mainContent.textContent = mainContent.textContent.slice(0, -3)
+//    mainContent.textContent = mainContent.textContent + hiddenContent.textContent.slice(0, Math.min(100, hiddenContent.textContent.length))
+//    if (hiddenContent.textContent.length > 100) {
+//        mainContent.innerHTML = mainContent.textContent + '...';
+//        hiddenContent.textContent = maintext.slice(100, maintext.length);
+//    } else {
+//        mainContent.innerHTML = mainContent.textContent;
+//        hiddenContent.textContent = '';
+//        showmoreButton.innerHTML = 'Show Less <i class="fas fa-arrow-up"></i>'
+//    }
+//}
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    var mainContent = document.getElementById('mainContent');
+//    var hiddenContent = document.getElementById('hiddenContent');
+//    var showmoreButton = document.getElementById('showButton');
+//    // Check if the content is already truncated
+//    if (mainContent.textContent.length > 100) {
+//        // Display only the first 500 characters
+//        hiddenContent.textContent = mainContent.textContent.slice(100, mainContent.textContent.length);
+//        mainContent.innerHTML = mainContent.textContent.slice(0, 100) + '...';
+//        // Display the "Show More" link
+//        hiddenContent.style.display = 'none';
+//    } else {
+//        mainContent.innerHTML = mainContent.textContent;
+//        hiddenContent.style.display = 'none';
+//        showmoreButton.style.display = 'none';
+//    }
+//});
+
+
+
+
+
+
+
+
 function showMore() {
     var mainContent = document.getElementById('mainContent');
     var hiddenContent = document.getElementById('hiddenContent');
     var showmoreButton = document.getElementById('showButton');
-    // Reveal the hidden content
-    console.log(hiddenContent.textContent);
-    if (mainContent.textContent.length < hiddenContent.textContent.length) {
+    if (hiddenContent.style.display == 'none') {
         showmoreButton.innerHTML = 'Show Less <i class="fas fa-arrow-up"></i>'
+        hiddenContent.style.display = 'inline'
+        mainContent.style.display = 'none'
     }
     else {
         showmoreButton.innerHTML = 'Show More <i class="fas fa-arrow-down"></i>'
+        hiddenContent.style.display = 'none'
+        mainContent.style.display = 'inline'
+
     }
-    var maintext = mainContent.textContent;
-    mainContent.innerHTML = hiddenContent.textContent;
-    hiddenContent.textContent = maintext;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     var mainContent = document.getElementById('mainContent');
     var hiddenContent = document.getElementById('hiddenContent');
-
+    var showmoreButton = document.getElementById('showButton');
     // Check if the content is already truncated
-    if (mainContent.textContent.length > 100) {
+    if (hiddenContent.textContent.length > 200) {
         // Display only the first 500 characters
-        hiddenContent.textContent = mainContent.textContent;
-        mainContent.innerHTML = mainContent.textContent.slice(0, 100) + '...';
+        mainContent.innerHTML = hiddenContent.textContent.slice(0, 200) + '...';
         // Display the "Show More" link
         hiddenContent.style.display = 'none';
+        mainContent.style.display = 'inline'
+    } else {
+        hiddenContent.style.display = 'inline'
+        showmoreButton.style.display = 'none';
     }
 });
