0616350464883d201d60e73c5d65d63c1c4f0831
Tanmay Singh <tanmay.singh@incruiter.com>
2023-12-11 03:20:35 +0530

    Addition separate file for the second approach

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
-//function showMore() {
-//    var mainContent = document.getElementById('mainContent');
-//    var hiddenContent = document.getElementById('hiddenContent');
-//    var showmoreButton = document.getElementById('showButton');
-//    if (hiddenContent.textContent.length == 0) {
-//        hiddenContent.textContent = mainContent.textContent.slice(0, mainContent.textContent.length);
-//        mainContent.textContent = '...';
-//   //     mainContent.textContent = mainContent.textContent.slice(0, 100) + '...';
-//        showmoreButton.innerHTML = 'Show More <i class="fas fa-arrow-down"></i>'
-//    }
-//    var maintext = hiddenContent.textContent;
-//    mainContent.textContent = mainContent.textContent.slice(0, -3)
-//    mainContent.textContent = mainContent.textContent + hiddenContent.textContent.slice(0, Math.min(100, hiddenContent.textContent.length))
-//    if (hiddenContent.textContent.length > 100) {
-//        mainContent.innerHTML = mainContent.textContent + '...';
-//        hiddenContent.textContent = maintext.slice(100, maintext.length);
-//    } else {
-//        mainContent.innerHTML = mainContent.textContent;
-//        hiddenContent.textContent = '';
-//        showmoreButton.innerHTML = 'Show Less <i class="fas fa-arrow-up"></i>'
-//    }
-//}
 
-//document.addEventListener("DOMContentLoaded", function () {
-//    var mainContent = document.getElementById('mainContent');
-//    var hiddenContent = document.getElementById('hiddenContent');
-//    var showmoreButton = document.getElementById('showButton');
-//    // Check if the content is already truncated
-//    if (mainContent.textContent.length > 100) {
-//        // Display only the first 500 characters
-//        hiddenContent.textContent = mainContent.textContent.slice(100, mainContent.textContent.length);
-//        mainContent.innerHTML = mainContent.textContent.slice(0, 100) + '...';
-//        // Display the "Show More" link
-//        hiddenContent.style.display = 'none';
-//    } else {
-//        mainContent.innerHTML = mainContent.textContent;
-//        hiddenContent.style.display = 'none';
-//        showmoreButton.style.display = 'none';
-//    }
-//});
-
-
-
-
-
-
-
+/* Second Approach */
 
 function showMore() {
     var mainContent = document.getElementById('mainContent');
